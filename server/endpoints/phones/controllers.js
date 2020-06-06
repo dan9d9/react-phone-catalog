7243f8c343ae5158116eb5f598a6f10d1caa519b
Daniel Dick <dan9d9@gmail.com>
2020-06-06 16:51:24 +0200

    simulate response time in controller

--- server/endpoints/phones/controllers.js
+++ server/endpoints/phones/controllers.js
@@ -5,16 +5,31 @@
 
 const phone_list = require('../../phones.json');
 
+
 class PhoneController {
 
   // Get All Phones //
   getPhones = async (req, res) => {
+    // Choose random number between 1 and 5 for setTimeout 
+    const waitTime = Math.floor(Math.random() * 5 + 1) * 1000;
+
     try{
-        // const phones = await Phones.find({});  // Phones model for demo only
-        res.status(200).json(phone_list);
+      // const phones = await Phones.find({});  // Demo of fetching phones from DB
+
+
+      // Simulate response time 
+      await new Promise(resolve => setTimeout(resolve, waitTime));
+
+      // Simulate error on unsuccesful request
+      // if(waitTime === 5000) {
+      //   throw new Error('Something went wrong!');
+      // }
+
+
+      res.status(200).json(phone_list);
     }
     catch(err){
-      console.log('Something went wrong!');
+      console.log(err);
       res.json({OK: false, message: err});
     }
   }
